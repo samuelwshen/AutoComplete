910b213454aa5b9576137734f2784dbb9f24a31a
Samuel Shen <samuelshen@berkeley.edu>
2018-10-18 12:05:15 -0700

    Reformatted EditDistance class

--- EditDistance.java
+++ EditDistance.java
@@ -2,12 +2,18 @@ import java.util.Collections;
 import java.util.Arrays;
 
 public class EditDistance {
-    public static void main (String [] args) {
-        String s1 = "Word1";
-        String s2= "WoRd22";
+    private int[][] arr;
+    private String s1;
+    private String s2;
 
-        int[][] arr = new int[s1.length()][s2.length()];
+    public EditDistance(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+        arr = new int[s1.length()][s2.length()];
+    }
 
+    public int getDist() {
+        //haven't calculated yet
         for (int i = 0; i < s1.length(); i++) {
             arr[i][0] = i;
         }
@@ -21,7 +27,7 @@ public class EditDistance {
                 arr[i][j] = Collections.min(Arrays.asList(arr[i-1][j] + 1, arr[i][j-1] + 1, arr[i-1][j-1] + diff(s1, s2, i, j)));
             }
         }
-        System.out.println(arr[s1.length() - 1][s2.length() - 1]);
+        return arr[s1.length() - 1][s2.length() - 1];
     }
 
     private static int diff(String s1, String s2, int index1, int index2) {
